d73ac5ed3a7ffb070f492600acd4639c7b629555
jasonkritter <87248779+jasonkritter@users.noreply.github.com>
2021-12-13 20:47:35 -0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+
 # build
-npm install
 npm run build
+
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
